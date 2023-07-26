d1b93e6c44e8515ac5a710a0ed654d5e7e53d0a0
“mavine4512” <mavineoyugi@gmail.com>
2023-07-26 12:46:30 -0700

    bug fixing

--- get_env.c
+++ get_env.c
@@ -8,8 +8,11 @@
  */
 char **get_environ(info_t *info)
 {
-	(!info->environ || info->env_changed) ?
-		(info->environ = list_to_strings(info->env), info->env_changed = 0) : 0;
+	if (!info->environ || info->env_changed)
+	{
+		info->environ = list_to_strings(info->env);
+		info->env_changed = 0;
+	}
 
 	return (info->environ);
 }
